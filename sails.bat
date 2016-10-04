170812e58699bba596572f7c65d8d4baeccf8c46
Robin Hossain <robinhossainbd@gmail.com>
2016-10-04 19:45:24 +0600

    Create sails.bat

---
+++ sails.bat
@@ -0,0 +1,20 @@
+sails new app
+cd app
+npm install
+sails lift
+sails generate controller Pages add edit destroy show index
+
+npm config set msvs_version 20xx --global
+
+//Run as Administrator.
+//Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+npm install -g npm-windows-upgrade
+npm-windows-upgrade
+//https://github.com/felixrieseberg/npm-windows-upgrade
+
+
+// https://github.com/nodejs/node-gyp#installation
+npm install -g node-gyp
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\Setenv.cmd" /Release /x86
+
+C:\Python27
