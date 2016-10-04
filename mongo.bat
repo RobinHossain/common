6da64e40f4d89c5ebbc3c3a16d74ba6f2073f277
Robin Hossain <robinhossainbd@gmail.com>
2016-10-04 19:44:36 +0600

    Create mongo.bat

---
+++ mongo.bat
@@ -0,0 +1,8 @@
+show dbs
+show databases
+use sochara
+db.collection
+db.user
+db.user.find()
+db.user.insert({"username":"tojibon", "email":"tojibon@gmail.com", "password":"tojibon"})
+db.user.find()
